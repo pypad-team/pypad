a53028fe56b68f96a46c893762009357a2f6cff0
Jason Kung <jkung101@mit.edu>
2020-05-02 04:24:38 -0400

    Added methods to editor

--- client/editor.ts
+++ client/editor.ts
@@ -4,21 +4,73 @@ declare const ace: any;
  * Editor wrapper to handle front-end display of data
  */
 class Editor {
+    private client: Client;
     private editor: any;
     private theme: string;
     private mode: string;
+    private enabled: boolean;
+    private cursors: Array<any>;
 
-    constructor(elementID = "editor", theme = "solarized_dark", mode = "python") {
+    constructor(client: any, elementID = "editor", theme = "solarized_dark", mode = "python") {
+        this.client = client;
         this.editor = ace.edit(elementID);
         this.theme = theme;
         this.mode = mode;
+        this.enabled = false;
+        this.cursors = new Array<any>();
 
         this.setTheme(theme);
         this.setMode(mode);
+        this.listenLocalChanges();
+        this.listenCursorChanges();
+    }
+
+    /**
+     * Insert text into the editor at given row and column of the editor
+     *
+     * @param text - Text to insert into editor
+     * @param row - Row (line number) to insert text at
+     * @param column - Column (char number in line) to insert text at
+     */
+    public insert(text: string, row: number, column: number): void {
+        const position = {
+            row: row,
+            column: column
+        };
+        this.editor.session.insert(position, text);
+    }
+
+    /**
+     * Set the cursors to be displayed in the editor
+     *
+     * @param cursors - Array of cursors from other peers to display in editor
+     */
+    public setCursors(cursors: Array<any>): void {
+        this.cursors = cursors;
+        cursors.forEach((cursor: any): void => {
+            // TODO: add to editor
+        });
+    }
+
+    /**
+     * Enable the editor to allow writing
+     */
+    public enable(): void {
+        this.editor.setReadOnly(false);
+        this.enabled = true;
+    }
+
+    /**
+     * Disable the editor to disallow writing
+     */
+    public disable(): void {
+        this.editor.setReadOnly(true);
+        this.enabled = false;
     }
 
     /**
      * Set the theme for the editor
+     *
      * @param theme - theme for the editor
      */
     public setTheme(theme: string): void {
@@ -28,6 +80,7 @@ class Editor {
 
     /**
      * Set the mode for the editor
+     *
      * @param mode - mode for the editor
      */
     public setMode(mode: string): void {
@@ -36,19 +89,30 @@ class Editor {
     }
 
     /**
-     * Set the text of the editor
-     * @param text - text to set editor to display
+     * Listen for local changes in editor to update CRDT
      */
-    public setText(text: string): void {
-        this.editor.setValue(text);
+    private listenLocalChanges(): void {
+        this.editor.on(
+            "change",
+            ((delta: any): void => {
+                // Filter out changes from insert method
+                if (this.editor.curOp && this.editor.curOp.command.name) {
+                    // TODO: update CRDT and send messages
+                }
+            }).bind(this)
+        );
     }
 
     /**
-     * Listen for local changes in editor
+     * Listen for cursor changes to emit to peers
      */
-    private listenLocalChanges(): void {
-        this.editor.session.on("change", function(delta: any) {
-            // TODO: send delta to CRDT
-        });
+    private listenCursorChanges(): void {
+        this.editor.selection.on(
+            "changeCursor",
+            ((): void => {
+                const cursorPosition = this.editor.getCursorPosition();
+                // TODO: emit new cursor position to connection
+            }).bind(this)
+        );
     }
 }
